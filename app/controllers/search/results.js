04c2c342e05cab569b2dbc19385c93367dea8e8f
Taras Mankovski <tarasm@gmail.com>
2013-09-17 18:38:30 +0200

    Removed unecessary controller

--- app/controllers/search/results.js
+++
@@ -1,8 +0,0 @@
-var ResultsSearchController = Ember.ArrayController.extend({
-  needs: ['search'],
-  content: function(){
-    console.log( this.get('controllers.search.keyword') );
-  }.property('controllers.search.keyword')
-});
-
-export default ResultsSearchController;
